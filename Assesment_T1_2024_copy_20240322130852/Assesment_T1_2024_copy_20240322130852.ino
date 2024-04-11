2fc02f89c46f773a63eb3a3b22562d7b86428ffa
User97212 <149123008+User97212@users.noreply.github.com>
2024-04-12 10:57:13 +1200

    Update Assesment_T1_2024_copy_20240322130852.ino
    changing teacher comments

--- Assesment_T1_2024_copy_20240322130852/Assesment_T1_2024_copy_20240322130852.ino
+++ Assesment_T1_2024_copy_20240322130852/Assesment_T1_2024_copy_20240322130852.ino
@@ -3,16 +3,12 @@
 #include "LibPrintf.h"
 #include <SparkFun_LPS25HB_Arduino_Library.h>  // include library for sensor
 
-//TEACHER COMMENT tell me what these are in a comment?
-LPS25HB pSensor;                               // create an object of the LPS25HB class
-OpenLog sdCard;
+LPS25HB pSensor; // create an object of the LPS25HB class, and enables the board to communicate with and retrieve data from the LPS25HB sensor
+OpenLog sdCard; // create an object of the OpenLog module object, which enables the board to communicate with the OpenLog module and log the data to an SD card
 
 const String CSVFILE = "data.csv";
 const byte SWTPIN = 2;
 int swtState = 0; // variable defining switche's state  (0 or 1/on or off)
-
-//TEACHER COMMENT is this used anywhere?
-const byte OpenLogAddress = 42;  // default Qwiic OpenLog I2C address
 
 void setup() {
   pinMode(SWTPIN, INPUT_PULLUP);
@@ -22,55 +18,48 @@ void setup() {
   sdCard.begin();  // open connection to the openLog
   sdCard.create(CSVFILE);
   sdCard.append(CSVFILE);
+  Serial.begin(9600); // initialise serial communication, baud rate set to 9600
   
-  // test to see if it is connected
+  // check to see if sdcard is connected
   sdCard.println("reading working: this is were you should see your pressure values");
   sdCard.syncFile();
-  
-  // initialise serial communication, baud rate set = 9600
-  Serial.begin(9600);
   Serial.print("sdCard should have a file containing the text 'reading working: this is were you should see your pressure values'");
  
-  // if it isn't connected, display this on the serial monitor
+  // if sensor isn't connected, display this on the serial monitor
   if (pSensor.isConnected() == false) {
-    Serial.print("Sensor Error, check your connections");                      // alerting the user that the sensor cannot be reached
-    //TEACHER COMMENT you might give a web address but if this error message is from the example I would delete it
-    Serial.println("See Example2_I2C_Configuration for how to change these");  // giving a suggestion for a solution
+    Serial.print("Error in sensor, check your connections"); // alerting the user that the sensor cannot be reached
   }
 
   //TEACHER COMMENT any check for the sdcard?
 
 }
 
-
 void loop() {
   swtState = digitalRead(SWTPIN);
    if (swtState == LOW) {
     Serial.print("Switch is OFF, turn on to work rocket");
    }
-    if (swtState == HIGH) { //TEACHER COMMENT not an else statement?
+    else if (swtState == HIGH) { 
     Serial.print("Switch is ON, rocket should work");
   // print the pressure reading in hPa
   sdCard.print("Pressure in hPa: ");
   sdCard.print(pSensor.getPressure_hPa());
   Serial.print("Pressure in hPa: ");
   Serial.println(pSensor.getPressure_hPa());
-  delay(1000); //TEACHER COMMENT why are you waiting a second between pressure, temperature, and time readings?
+  delay(1000); // wait a second so that the values on the sdcard are readable and not too fast
 
   // print the temperature in degrees C
   sdCard.print(", Temperature (degC): ");
   sdCard.print(pSensor.getTemperature_degC());
   Serial.print(", Temperature (degC): ");
   Serial.println(pSensor.getTemperature_degC());
-  delay(1000);//TEACHER COMMENT why are you waiting a second between pressure, temperature, and time readings?
-
+  delay(1000); // wait a second so that the values on the sdcard are readable and not too fast
 
   // print the amount of ms since the rocket took off
   sdCard.print(", Time: ");
   sdCard.print(millis());
   Serial.print(", Time: ");
   Serial.println(millis());
-//TEACHER COMMENT if 40 is as quick as it can go why do you have 1000?
-  delay(1000);  // wait - 40 ms is the maximum update rate of the sensor (25 Hz)
+  delay(1000);  // wait a second so that the values on the sdcard are readable and not too fast
 }
   }
