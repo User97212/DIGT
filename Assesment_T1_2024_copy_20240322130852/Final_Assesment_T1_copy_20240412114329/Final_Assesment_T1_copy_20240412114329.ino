1b6ecfcfab1707e353540f80aadf7ee17457f3d2
User97212 <21279@orewacollege.nz>
2024-05-07 09:36:22 +1200

    Update Final_Assesment_T1_copy_20240412114329.ino

--- Assesment_T1_2024_copy_20240322130852/Final_Assesment_T1_copy_20240412114329/Final_Assesment_T1_copy_20240412114329.ino
+++ Assesment_T1_2024_copy_20240322130852/Final_Assesment_T1_copy_20240412114329/Final_Assesment_T1_copy_20240412114329.ino
@@ -1,17 +1,16 @@
 #include <Wire.h>
-#include <SparkFun_Qwiic_OpenLog_Arduino_Library.h> // includes library for sdCard
-#include "LibPrintf.h" // includes library for CSV file
-#include <SparkFun_LPS25HB_Arduino_Library.h>  // includes library for sensor
-#include <Servo.h> // include servo library 
+#include <SparkFun_Qwiic_OpenLog_Arduino_Library.h>  // includes library for sdCard
+#include "LibPrintf.h"                               // includes library for CSV file
+#include <SparkFun_LPS25HB_Arduino_Library.h>        // includes library for sensor
+#include <Servo.h>                                   // include servo library
 
-Servo parachuteServo; // create an object that declares the servo that controls the parachute when the rocket is coming down
-LPS25HB pSensor;  // create an object of the LPS25HB class, and enables the board to communicate with and retrieve data from the LPS25HB sensor
-OpenLog sdCard;   // create an object of the OpenLog module object, which enables the board to communicate with the OpenLog module and log the data to an SD card
+Servo parachuteServo;  // create an object that declares the servo that controls the parachute when the rocket is coming down
+LPS25HB pSensor;       // create an object of the LPS25HB class, and enables the board to communicate with and retrieve data from the LPS25HB sensor
+OpenLog sdCard;        // create an object of the OpenLog module object, which enables the board to communicate with the OpenLog module and log the data to an SD card
 
 const String CSVFILE = "data.csv";
 const byte SWTPIN = 2;
 int swtState = 0;  // variable defining switche's state  (0 or 1/on or off)
-bool parachuteDeploy = false;
 
 void setup() {
   pinMode(SWTPIN, INPUT_PULLUP);
@@ -23,6 +22,7 @@ void setup() {
   sdCard.append(CSVFILE);
   Serial.begin(9600);  // initialise serial communication, baud rate set to 9600
   parachuteServo.attach(4);
+  parachuteServo.write(0);  // default/initial position of the servo is 0 degrees
 
   // check to see if sdcard is connected
   sdCard.println("reading working: this is were you should see your pressure values");
@@ -41,7 +41,15 @@ void setup() {
   if (status < 0x10) Serial.print("0");  // add a leading zero if the status byte is less than 16 for consistent formatting
   Serial.println(status, HEX);           // print the status byte in hexadecimal format and move to a new line on the serial monitor
 }
+void parachuteDeploy() {
+  float getPressure = pSensor.getPressure_hPa();  // get the current pressure reading from the sensor
 
+  if (getPressure <= 250) {     // if that pressure reading is higher than or equal to 250hPa, go to next line
+    parachuteServo.write(180);  // deploy parachute (assuming servo controls parachute deployment)
+  } else {                      // if pressure reading is lower than 250hPa, go to next line
+    parachuteServo.write(0);    // turn keep servo at 0 degrees (keep parachute closed)
+  }
+}
 void loop() {
   swtState = digitalRead(SWTPIN);
   if (swtState == LOW) {
@@ -68,11 +76,6 @@ void loop() {
     Serial.print(", Time: ");
     Serial.println(millis());
     delay(1000);  // wait a second so that the values on the sdcard are readable and not too fast
+    parachuteDeploy();
   }
-  if (parachuteDeploy) {
-    parachuteServo.write(180); // Move servo to 180 degrees
-  } else {
-    parachuteServo.write(0); // Move servo to 0 degrees
-  }
-
 }
